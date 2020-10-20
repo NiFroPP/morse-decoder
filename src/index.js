76a219afc8e807cd2e85274abd69f3505f3b7317
Nick Frolov <frolovnick0@gmail.com>
2020-10-20 18:55:40 +0300

    init: add function

--- src/index.js
+++ src/index.js
@@ -37,9 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+    function decode(expr) {
+        let arr = [];
+    
+        while (expr.length > 0) {
+            let symbol = '';
+            let letter = '';
+            let str = expr.slice(0, 10);
+            for (let i = 0; i < str.length; i++) {
+                symbol += str[i];
+                if (symbol === '00') {
+                    symbol = '';
+                    continue;
+                } else if (symbol === '10') {
+                    letter += '.';
+                    symbol = '';
+                    continue;
+                } else if (symbol === '11') {
+                    letter += '-';
+                    symbol = '';
+                    continue;
+                }
+            }
+            arr.push(letter);
+            expr = expr.slice(10);
+        }
+    
+        return arr.reduce((prev, item) => {
+            return prev += item === '' ? ' ' : MORSE_TABLE[item];
+        }, '')
+    }
 
 module.exports = {
     decode
